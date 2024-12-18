fd56e62b4bafda7e9009c5dda1f589ab82e5946e
Ricardo Oliveira <ricvenicius@gmail.com>
2024-12-18 14:30:21 -0300

    feat(users): add session-based authentication views

---
+++ users/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from users import views
+
+app_name = "users"
+
+urlpatterns = [
+    path("", views.profile, name="profile"),
+
+    path("cadastro", views.register, name="register"),
+
+    path('logar', views.login_view, name="login_view"),
+    path('logar/criar', views.login_create, name="login_create"),
+
+    path("logout/", views.logout_execute, name="logout"),
+]
