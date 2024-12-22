d3ff8b6f72425fc73a2a3454da72a61b5277482d
Ricardo Oliveira <ricvenicius@gmail.com>
2024-12-21 23:18:31 -0300

    feat: create change password template

--- users/forms/__init__.py
+++ users/forms/__init__.py
@@ -2,3 +2,4 @@
 from .login_form import LoginForm  # noqa
 from .register_form import RegisterForm  # noqa
 from .update_form import UpdateUserForm  # noqa
+from .update_password import UpdatePasswordForm  # noqa
