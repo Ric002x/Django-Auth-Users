2f21beae66d43468475b4bc4a3ab1264998b3b3c
Ricardo Oliveira <ricvenicius@gmail.com>
2024-12-22 20:11:07 -0300

    fix: add default class for user authentication

--- setup/settings.py
+++ setup/settings.py
@@ -146,6 +146,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Rest Framework Settings
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated'
     ]
