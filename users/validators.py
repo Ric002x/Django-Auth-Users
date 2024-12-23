97924473fd4d3b2c9e3097c42d4c9954045b440f
Ricardo Oliveira <ricvenicius@gmail.com>
2024-12-23 16:35:36 -0300

    refactor validator in users

--- users/validators.py
+++ users/validators.py
@@ -11,6 +11,7 @@ class RegisterUserValidator:
         self.data: dict = data
         self.errors = defaultdict(list) if errors is None else errors
         self.ErrorClass = ValidationError if ErrorClass is None else ErrorClass
+        self.fields = ['name', 'email', 'password', 'repeat_password']
         self.execute_clean()
 
     def strong_password(self, password):
@@ -23,28 +24,16 @@ class RegisterUserValidator:
             )
 
     def execute_clean(self, *args, **kwargs):
-        if not self.data.get('name'):
-            self.errors['name'].append(
-                "Este campo é obrigatório"
-            )
-        if not self.data.get('email'):
-            self.errors['email'].append(
-                "Este campo é obrigatório"
-            )
-        if not self.data.get('password'):
-            self.errors['password'].append(
-                "Este campo é obrigatório"
-            )
-        if not self.data.get('repeat_password'):
-            self.errors['repeat_password'].append(
-                "Este campo é obrigatório"
-            )
+        for field in self.fields:
 
-        if self.errors:
-            raise self.ErrorClass(self.errors)  # type: ignore
+            if not self.data.get(field):
+                self.errors[field].append(
+                    "Este campo é obrigatório"
+                )
 
         self.clean_email()
         self.clean_password()
+        self.clean_avatar()
 
         if self.errors:
             raise self.ErrorClass(self.errors)  # type: ignore
@@ -67,6 +56,9 @@ class RegisterUserValidator:
             self.errors['password'].append(
                 "As senhas não coincidem"
             )
+
+    def clean_avatar(self):
+        pass
 
 
 class LoginUserValidator:
@@ -98,20 +90,8 @@ class LoginUserValidator:
 class UpdateUserValidator(RegisterUserValidator):
 
     def execute_clean(self, *args, **kwargs):
-        if not self.data.get('name'):
-            self.errors['name'].append(
-                "Este campo é obrigatório"
-            )
-        if not self.data.get('email'):
-            self.errors['email'].append(
-                "Este campo é obrigatório"
-            )
-
-        self.clean_email()
-        self.clean_avatar()
-
-        if self.errors:
-            raise self.ErrorClass(self.errors)  # type: ignore
+        self.fields = ['name', 'email']
+        return super().execute_clean(*args, **kwargs)
 
     def clean_email(self, *args, **kwargs):
         email = self.data.get('email')
@@ -156,20 +136,20 @@ class UpdatePasswordValidator:
         regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[1-9]).{8,}$')
 
         if not regex.match(password):
-            self.errors['new_password'].append(self.ErrorClass(
+            self.errors['new_password'].append(
                 'A senha deve contar pelo menos 8 caracteres, '
-                'incluindo letras maiúsculas e números',
-                code='invalid',
-            ))
+                'incluindo letras maiúsculas e números'
+            )
 
     def execute_clean(self, *args, **kwargs):
-        self.clean_password()
-
-        for key, value in self.data.items():
-            if not self.data.get(key):
-                self.errors[key].append(self.ErrorClass(
+        fields = ['old_password', 'new_password', 'repeat_password']
+        for i in fields:
+            if not self.data.get(i):
+                self.errors[i].append(
                     "Este campo é obrigatório"
-                ))
+                )
+
+        self.clean_password()
 
         if self.errors:
             raise self.ErrorClass(self.errors)  # type: ignore
@@ -181,6 +161,6 @@ class UpdatePasswordValidator:
         self.strong_password(new_password)
 
         if new_password != repeat_password:
-            self.errors['new_password'].append(self.ErrorClass(
+            self.errors['new_password'].append(
                 "As senhas não coincidem"
-            ))
+            )
