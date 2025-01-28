861adc1177f20a5d1c9fe95efd0e340048c57c79
Ricardo Oliveira <ricvenicius@gmail.com>
2025-01-28 17:38:04 -0300

    removing unnecessary files

--- users/urls.py
+++ users/urls.py
@@ -5,16 +5,6 @@ from users import views
 app_name = "users"
 
 urlpatterns = [
-    path("", views.profile, name="profile"),
-
-    path("register", views.register, name="register"),
-    path("login", views.login_, name="login"),
-    path("logout", views.logout_execute, name="logout"),
-
-    path("update", views.update_profile, name="update_profile"),
-    path('password', views.update_password, name="update_password"),
-
-
     path('api/v1/register', views.CreateUserAPIView.as_view()),
     path('api/v1/login', views.LoginUserAPIView.as_view()),
 
--- users/views/__init__.py
+++
@@ -1,2 +0,0 @@
-from .api import *  # noqa
-from .site import *  # noqa
