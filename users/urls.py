be81b4604f927698357b64ebbec09e74c8583d37
Ricardo Oliveira <ricvenicius@gmail.com>
2025-02-04 17:36:04 -0300

    refactor: update url syntax

--- users/urls.py
+++ users/urls.py
@@ -5,9 +5,11 @@ from users import views
 app_name = "users"
 
 urlpatterns = [
-    path('api/v1/register', views.CreateUserAPIView.as_view()),
-    path('api/v1/login', views.LoginUserAPIView.as_view()),
+    path('register', views.CreateUserAPIView.as_view()),
+    path('login', views.LoginUserAPIView.as_view()),
 
-    path("api/v1/me", views.UserAPIView.as_view()),
-    path('api/v1/me/password', views.UserChangePasswordAPIView.as_view())
+    path("me", views.UserAPIView.as_view()),
+    path('me/password', views.UserChangePasswordAPIView.as_view()),
+
+    path('auth/google', views.OAuthLogin.as_view()),
 ]
